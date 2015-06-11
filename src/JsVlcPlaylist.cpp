05f336d75a4e7cf52ce6f0ef153074546907c011
Sergey Radionov <RSATom@gmail.com>
2015-06-11 22:17:33 +0600

    JsVlcPlaylist: allow set playlist mode

--- src/JsVlcPlaylist.cpp
+++ src/JsVlcPlaylist.cpp
@@ -74,3 +74,47 @@ void JsVlcPlaylist::jsCreate( const v8::FunctionCallbackInfo<v8::Value>& args )
             constructor->NewInstance( sizeof( argv ) / sizeof( argv[0] ), argv ) );
     }
 }
+
+void JsVlcPlaylist::jsMode( v8::Local<v8::String> property,
+                            const v8::PropertyCallbackInfo<v8::Value>& info )
+{
+    using namespace v8;
+
+    Isolate* isolate = Isolate::GetCurrent();
+    HandleScope scope( isolate );
+
+    JsVlcPlaylist* playlist = ObjectWrap::Unwrap<JsVlcPlaylist>( info.Holder() );
+
+    info.GetReturnValue().Set(
+        Integer::New( isolate,
+                      static_cast<int>( playlist->_jsPlayer->player().get_playback_mode() ) ) );
+}
+
+void JsVlcPlaylist::jsSetMode( v8::Local<v8::String> property,
+                               v8::Local<v8::Value> value,
+                               const v8::PropertyCallbackInfo<void>& info )
+{
+    using namespace v8;
+
+    Isolate* isolate = Isolate::GetCurrent();
+    HandleScope scope( isolate );
+
+    JsVlcPlaylist* jsPlaylist = ObjectWrap::Unwrap<JsVlcPlaylist>( info.Holder() );
+
+    vlc::player& player = jsPlaylist->_jsPlayer->player();
+
+    Local<Integer> jsMode = Local<Integer>::Cast( value );
+    if( !jsMode.IsEmpty() ) {
+        switch( jsMode->Value() ) {
+            case static_cast<decltype( jsMode->Value() )>( PlaybackMode::Normal ):
+                player.set_playback_mode( vlc::mode_normal );
+                break;
+            case static_cast<decltype( jsMode->Value() )>( PlaybackMode::Loop ):
+                player.set_playback_mode( vlc::mode_loop );
+                break;
+            case static_cast<decltype( jsMode->Value() )>( PlaybackMode::Single ):
+                player.set_playback_mode( vlc::mode_single );
+                break;
+        }
+    }
+}
--- src/JsVlcPlaylist.h
+++ src/JsVlcPlaylist.h
@@ -22,6 +22,13 @@ public:
     static void initJsApi();
     static void jsCreate( const v8::FunctionCallbackInfo<v8::Value>& args );
 
+    static void jsMode( v8::Local<v8::String> property,
+                        const v8::PropertyCallbackInfo<v8::Value>& info );
+    static void jsSetMode( v8::Local<v8::String> property,
+                           v8::Local<v8::Value> value,
+                           const v8::PropertyCallbackInfo<void>& info );
+
+
 private:
     JsVlcPlaylist( v8::Local<v8::Object>& thisObject, JsVlcPlayer* );
 
