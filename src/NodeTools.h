b1d504474613637e67ec667e25e4384c09a0dd47
Sergey Radionov <RSATom@gmail.com>
2015-06-14 17:55:52 +0600

    Mac: build fixed

--- src/NodeTools.h
+++ src/NodeTools.h
@@ -3,19 +3,19 @@
 #include "Tools.h"
 
 template<typename T>
-T FromJsValue( v8::Local<v8::Value>& value )
+inline T FromJsValue( const v8::Local<v8::Value>& value )
 {
     return typename T::Cast( value );
 }
 
 template<>
-inline v8::Local<v8::Value> FromJsValue<v8::Local<v8::Value> >( v8::Local<v8::Value>& value )
+inline v8::Local<v8::Value> FromJsValue<v8::Local<v8::Value> >( const v8::Local<v8::Value>& value )
 {
     return value;
 }
 
 template<>
-inline std::string FromJsValue<std::string>( v8::Local<v8::Value>& value )
+inline std::string FromJsValue<std::string>( const v8::Local<v8::Value>& value )
 {
     v8::String::Utf8Value str( value->ToString() );
 
@@ -23,19 +23,19 @@ inline std::string FromJsValue<std::string>( v8::Local<v8::Value>& value )
 }
 
 template<>
-inline unsigned FromJsValue<unsigned>( v8::Local<v8::Value>& value )
+inline unsigned FromJsValue<unsigned>( const v8::Local<v8::Value>& value )
 {
     return static_cast<unsigned>( v8::Local<v8::Integer>::Cast( value )->Value() );
 }
 
 template<>
-inline int FromJsValue<int>( v8::Local<v8::Value>& value )
+inline int FromJsValue<int>( const v8::Local<v8::Value>& value )
 {
     return static_cast<int>( v8::Local<v8::Integer>::Cast( value )->Value() );
 }
 
 template<>
-std::vector<std::string> FromJsValue<std::vector<std::string> >( v8::Local<v8::Value>& value )
+std::vector<std::string> FromJsValue<std::vector<std::string> >( const v8::Local<v8::Value>& value )
 {
     std::vector<std::string> result;
 
@@ -68,21 +68,26 @@ inline v8::Local<v8::Value> ToJsValue( unsigned value )
     return v8::Integer::New( v8::Isolate::GetCurrent(), value );
 }
 
-template<typename C, typename ... A, unsigned ... I >
+template<typename C, typename ... A, size_t ... I >
 void CallMethod( void ( C::* method) ( A ... ),
-                 const v8::FunctionCallbackInfo<v8::Value>& info, StaticSequence<I ...> )
+                 const v8::FunctionCallbackInfo<v8::Value>& info,
+                 StaticSequence<I ...> )
 {
     v8::Isolate* isolate = v8::Isolate::GetCurrent();
     v8::HandleScope scope( isolate );
 
     C* instance = node::ObjectWrap::Unwrap<C>( info.Holder() );
 
-    ( instance->*method ) ( FromJsValue<std::remove_const<std::remove_reference<A>::type>::type >( info[I] ) ... );
+    ( instance->*method ) (
+        FromJsValue<
+            typename std::remove_const<
+                typename std::remove_reference<A>::type>::type >( info[I] ) ... );
 };
 
-template<typename R, typename C, typename ... A, unsigned ... I >
+template<typename R, typename C, typename ... A, size_t ... I >
 void CallMethod( R ( C::* method) ( A ... ),
-                 const v8::FunctionCallbackInfo<v8::Value>& info, StaticSequence<I ...> )
+                 const v8::FunctionCallbackInfo<v8::Value>& info,
+                 StaticSequence<I ...> )
 {
     v8::Isolate* isolate = v8::Isolate::GetCurrent();
     v8::HandleScope scope( isolate );
@@ -92,13 +97,15 @@ void CallMethod( R ( C::* method) ( A ... ),
     info.GetReturnValue().Set(
         ToJsValue(
             ( instance->*method ) (
-                FromJsValue<std::remove_const<std::remove_reference<A>::type>::type >( info[I] ) ... ) ) );
+                FromJsValue<typename std::remove_const<typename std::remove_reference<A>::type>::type >( info[I] ) ... ) ) );
 };
 
 template<typename R, typename C, typename ... A>
-void CallMethod( R ( C::* method ) ( A ... ), const v8::FunctionCallbackInfo<v8::Value>& info )
+void CallMethod( R ( C::* method ) ( A ... ),
+                 const v8::FunctionCallbackInfo<v8::Value>& info )
 {
-    return CallMethod( method, info, MakeStaticSequence<sizeof ... ( A )>::Sequence() );
+    typedef typename MakeStaticSequence<sizeof ... ( A )>::SequenceType SequenceType;
+    CallMethod( method, info, SequenceType() );
 }
 
 template<typename R, typename C>
@@ -126,7 +133,7 @@ void SetPropertyValue( void ( C::* setter ) (V),
 
     C* instance = node::ObjectWrap::Unwrap<C>( info.Holder() );
 
-    typedef std::remove_const<std::remove_reference<V>::type>::type cleanPropType;
+    typedef typename std::remove_const<typename std::remove_reference<V>::type>::type cleanPropType;
     ( instance->*setter ) ( FromJsValue<cleanPropType>( value ) );
 }
 
--- src/Tools.h
+++ src/Tools.h
@@ -1,6 +1,6 @@
 #pragma once
 
-template<size_t ...>
+template<size_t ... Sequence>
 struct StaticSequence
 {
 };
@@ -11,17 +11,17 @@ struct StaticSequenceGenerator;
 template<size_t ... Sequence>
 struct StaticSequenceGenerator<0, Sequence ...>
 {
-    typedef StaticSequence<Sequence ...> Sequence;
+    typedef StaticSequence<Sequence ...> SequenceType;
 };
 
 template<size_t Current, size_t ... Sequence>
 struct StaticSequenceGenerator
 {
-    typedef typename StaticSequenceGenerator<Current - 1, Current - 1, Sequence ...>::Sequence Sequence;
+    typedef typename StaticSequenceGenerator<Current - 1, Current - 1, Sequence ...>::SequenceType SequenceType;
 };
 
 template<size_t Length>
 struct MakeStaticSequence
 {
-    typedef typename StaticSequenceGenerator<Length>::Sequence Sequence;
+    typedef typename StaticSequenceGenerator<Length>::SequenceType SequenceType;
 };
