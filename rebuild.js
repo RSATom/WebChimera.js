6d263d35b730e54b315af63134eb6640aca9ea75
Sergey Radionov <RSATom@gmail.com>
2015-07-01 00:42:13 +0600

    more flexable way configure runtime options used

--- rebuild.js
+++ rebuild.js
@@ -1,10 +1,27 @@
 var cmakeJS = require("cmake-js");
 
+var defaultRuntime = "nw";
+var defaultRuntimeVersion = "0.12.2";
+var defaultWinArch = "ia32";
+
 var options = {
-    runtime: process.env.npm_config_wcjs_runtime || null,
-    runtimeVersion: process.env.npm_config_wcjs_runtime_version || null,
-    arch: process.env.npm_config_wcjs_arch || null
+    runtime: process.env.npm_config_wcjs_runtime || undefined,
+    runtimeVersion: process.env.npm_config_wcjs_runtime_version || undefined,
+    arch: process.env.npm_config_wcjs_arch || undefined
 };
 
 var buildSystem = new cmakeJS.BuildSystem(options);
+
+if(buildSystem.options.runtime == undefined) {
+    buildSystem.options.runtime = defaultRuntime;
+}
+
+if(buildSystem.options.runtimeVersion == undefined) {
+    buildSystem.options.runtimeVersion = defaultRuntimeVersion;
+}
+
+if(buildSystem.options.arch == undefined && process.platform == "win32") {
+    buildSystem.options.arch = defaultWinArch;
+}
+
 buildSystem.rebuild();
