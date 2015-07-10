757cb15e188f69646e80ebd945a583104c59225b
Sergey Radionov <RSATom@gmail.com>
2015-07-10 11:32:08 +0600

    rebuild.js: try workaround not downloaded yet dependency problem
    thanks to Gábor Mező

--- rebuild.js
+++ rebuild.js
@@ -1,27 +1,52 @@
-var cmakeJS = require("cmake-js");
+function build() {
+    var cmakeJS = require("cmake-js");
 
-var defaultRuntime = "nw";
-var defaultRuntimeVersion = "0.12.2";
-var defaultWinArch = "ia32";
+    var defaultRuntime = "nw";
+    var defaultRuntimeVersion = "0.12.2";
+    var defaultWinArch = "ia32";
 
-var options = {
-    runtime: process.env.npm_config_wcjs_runtime || undefined,
-    runtimeVersion: process.env.npm_config_wcjs_runtime_version || undefined,
-    arch: process.env.npm_config_wcjs_arch || undefined
+    var options = {
+        runtime: process.env.npm_config_wcjs_runtime || undefined,
+        runtimeVersion: process.env.npm_config_wcjs_runtime_version || undefined,
+        arch: process.env.npm_config_wcjs_arch || undefined
+    };
+
+    var buildSystem = new cmakeJS.BuildSystem(options);
+
+    if (buildSystem.options.runtime == undefined) {
+        buildSystem.options.runtime = defaultRuntime;
+    }
+
+    if (buildSystem.options.runtimeVersion == undefined) {
+        buildSystem.options.runtimeVersion = defaultRuntimeVersion;
+    }
+
+    if (buildSystem.options.arch == undefined && process.platform == "win32") {
+        buildSystem.options.arch = defaultWinArch;
+    }
+
+    buildSystem.rebuild();
+}
+
+var times = 0;
+
+function begin() {
+    try {
+        build();
+    }
+    catch(e) {
+        if (e.code == "MODULE_NOT_FOUND") {
+            if (times++ == 5) {
+                throw e;
+            }
+            else {
+                setTimeout(begin, 2000);
+            }
+        }
+        else {
+            throw e;
+        }
+    }
 };
 
-var buildSystem = new cmakeJS.BuildSystem(options);
-
-if(buildSystem.options.runtime == undefined) {
-    buildSystem.options.runtime = defaultRuntime;
-}
-
-if(buildSystem.options.runtimeVersion == undefined) {
-    buildSystem.options.runtimeVersion = defaultRuntimeVersion;
-}
-
-if(buildSystem.options.arch == undefined && process.platform == "win32") {
-    buildSystem.options.arch = defaultWinArch;
-}
-
-buildSystem.rebuild();
+begin();
