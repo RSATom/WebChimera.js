98dc7661067c17d7860030ad81dc635485c2ced0
Ivo Georgiev <ivo@linvo.org>
2015-07-31 19:02:58 +0300

    added build scripts for node.js

---
+++ build_node.cmd
@@ -0,0 +1,5 @@
+set npm_config_wcjs_runtime="node"
+set npm_config_wcjs_runtime_version="0.12.7"
+set npm_config_wcjs_arch="ia32"
+
+npm install
---
+++ build_node.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export npm_config_wcjs_runtime="node"
+export npm_config_wcjs_runtime_version="0.12.7"
+
+npm install
