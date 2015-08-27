c17b0c2bd53f39917798cdef36dcc2469f64d13e
Sergey Radionov <RSATom@gmail.com>
2015-08-28 00:25:04 +0600

    use Electron v0.30.6
    in hope it's more stable

--- build_electron.cmd
+++ build_electron.cmd
@@ -1,5 +1,5 @@
 set npm_config_wcjs_runtime="electron"
-set npm_config_wcjs_runtime_version="0.31.0"
+set npm_config_wcjs_runtime_version="0.30.6"
 set npm_config_wcjs_arch="ia32"
 
 npm install
--- build_electron.sh
+++ build_electron.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export npm_config_wcjs_runtime="electron"
-export npm_config_wcjs_runtime_version="0.31.0"
+export npm_config_wcjs_runtime_version="0.30.6"
 
 npm install
