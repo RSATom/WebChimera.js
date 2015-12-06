fa45ab81d79162ff5823f591dae8d82e00585930
Sergey Radionov <RSATom@gmail.com>
2015-12-06 13:42:12 +0600

    travis-ci: build archive with vlc for OS X

---
+++ travis_before_deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+VLC_APP=./deps/VLC.app
+OUT_DIR=$BUILD_DIR/webchimera.js
+
+if [ ! $TRAVIS ]; then
+  exit 1
+fi
+
+zip -j $WCJS_ARCHIVE_PATH $BUILD_DIR/WebChimera.js.node
+
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+  mkdir $OUT_DIR
+
+  cp -f $BUILD_DIR/WebChimera.js.node $OUT_DIR
+  echo "module.exports = require('./WebChimera.js.node')" > $OUT_DIR/index.js
+
+  mkdir $OUT_DIR/lib
+  cp -R $VLC_APP/Contents/MacOS/lib/*.dylib $OUT_DIR/lib
+
+  mkdir -p $OUT_DIR/lib/vlc
+  cp -R $VLC_APP/Contents/MacOS/plugins $OUT_DIR/lib/vlc
+
+  mkdir -p $OUT_DIR/lib/vlc/share/lua
+  cp -R $VLC_APP/Contents/MacOS/share/lua/extensions $OUT_DIR/lib/vlc/share/lua
+  cp -R $VLC_APP/Contents/MacOS/share/lua/modules $OUT_DIR/lib/vlc/share/lua
+  cp -R $VLC_APP/Contents/MacOS/share/lua/playlist $OUT_DIR/lib/vlc/share/lua
+
+  mkdir -p $OUT_DIR/lib/vlc/lib
+  ln -sf ../../libvlccore.8.dylib $OUT_DIR/lib/vlc/lib/libvlccore.8.dylib
+  ln -sf ../../liblzma.5.dylib $OUT_DIR/lib/vlc/lib/liblzma.5.dylib
+
+  tar -cvzf $WCJS_FULL_ARCHIVE_PATH -C $BUILD_DIR webchimera.js
+
+  rm -rf $OUT_DIR
+fi
