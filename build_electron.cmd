61109f7754d87c285253d7a2631d499f17a71394
Sergey Radionov <RSATom@gmail.com>
2015-12-23 14:35:53 +0600

    build for Electron 0.33.9 as latest stable

--- build_electron.cmd
+++ build_electron.cmd
@@ -1,5 +1,5 @@
 set npm_config_wcjs_runtime="electron"
-set npm_config_wcjs_runtime_version="0.36.1"
+set npm_config_wcjs_runtime_version="0.33.9"
 set npm_config_wcjs_arch="ia32"
 
 npm install
--- build_electron.sh
+++ build_electron.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export npm_config_wcjs_runtime="electron"
-export npm_config_wcjs_runtime_version="0.36.1"
+export npm_config_wcjs_runtime_version="0.33.9"
 
 npm install
