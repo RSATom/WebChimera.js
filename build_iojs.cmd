6609034566dd7649f675f975aedd2f7aa06818b7
sergey radionov <rsatom@gmail.com>
2020-12-19 14:12:09 +0700

    iojs is dead long time already

--- build_iojs.cmd
+++
@@ -1,5 +0,0 @@
-set npm_config_wcjs_runtime="iojs"
-set npm_config_wcjs_runtime_version="3.1.0"
-set npm_config_wcjs_arch="ia32"
-
-npm install
--- build_iojs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export npm_config_wcjs_runtime="iojs"
-export npm_config_wcjs_runtime_version="3.1.0"
-
-npm install
